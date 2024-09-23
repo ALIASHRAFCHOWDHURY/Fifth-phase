9080970a5978688d32603953207e75e4342d5d04
Ashrafii381684 <aliashrafmd19@gmail.com>
2024-09-24 04:07:23 +0600

    faq part complete

--- features.js
+++ features.js
@@ -1,1 +1,11 @@
 console.log('js file clicked')
+
+
+
+
+
+
+<div class="flex text-gray-600 bg-gray-100 w-2/12 rounded-xl p-2">
+                                        <img src="dollar 1.png" alt="">
+                                        <p class="ps-2"><span>0</span> Bdt</p>
+                                </div>
