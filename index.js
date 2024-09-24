60ae46ed96b2c79840bf4ff1b407c14086e30060
Ashrafii381684 <aliashrafmd19@gmail.com>
2024-09-24 17:08:43 +0600

    dontion first part completed

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+/***Nav bar Blog */
+
 document.getElementById('blog-section').addEventListener('click', function(){
         window.location.href = 'home.html';
+})
+
+/***donation part */
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+        event.preventDefault();
+        const addMoney = getInputValueById('add-money-input') ;  
+        
+        if (isNaN(addMoney) || addMoney < 0) {
+                alert('failed to donate money!!');
+                return;
+            }
+            
+
+        const donationFund = getTextValueById('donation-fund');
+        const donatedMoney =  addMoney + donationFund;
+        const myAccountBalance = getTextValueById('account-balance');
+        const availableBalance = myAccountBalance - addMoney ;
+        document.getElementById('account-balance').innerText = availableBalance
+        document.getElementById('donation-fund').innerText = donatedMoney ;
+
 })
--- utilities.js
+++ utilities.js
@@ -1,1 +1,11 @@
-console.log('js file clicked')
+function getInputValueById(id){
+        const inputValue = document.getElementById(id).value ;
+        const inputNumber = parseFloat(inputValue)
+        return inputNumber ;
+}
+
+function getTextValueById(id){
+        const TextValue = document.getElementById(id).innerText ;
+        const TextNumber = parseFloat(TextValue)
+        return TextNumber ;
+}
