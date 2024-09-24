bfab126f4e34c44c425149f332c03d10f4d14f2b
Ashrafii381684 <aliashrafmd19@gmail.com>
2024-09-24 19:55:31 +0600

    gmt time complete

--- index.js
+++ index.js
@@ -82,4 +82,17 @@ document.getElementById('btn-add-money-three').addEventListener('click', functio
         document.getElementById('account-balance').innerText = availableBalance
         document.getElementById('donation-fund-three').innerText = donatedMoney ;
 
+                // added to history
+        const div = document.createElement('div');
+        div.classList.add('container', 'mx-auto',  'md:w-8/12',  'border-2',  'border-gray-200', 'flex', 'justify-center' ,'my-5' , 'p-3', 'rounded-xl')
+        div.innerHTML= `
+                <div>
+                        <h1 class='md:text-3xl text-xl font-bold'> Added ${addMoney} TK.donate for Flood at Noakhali, Bangladesh.Now your available balance ${availableBalance} tk. </h1>
+                        ${new Date()}
+                </div>
+        `
+        
+        document.getElementById('history-container').appendChild(div)
+
+        
 })
